5b2c899e130b4b47a30b8c27e6df5a474277e503
agui1era <aguileraelectro@gmail.com>
2021-07-21 12:38:07 -0400

    Cambio a mensaje push alertas

--- alertas.py
+++ alertas.py
@@ -1,11 +1,7 @@
 import psycopg2
-import smtplib, ssl
 import datetime
 import math 
-from email.mime.multipart import MIMEMultipart 
-from email.mime.text import MIMEText 
-import time
-import smtplib 
+from pushbullet import Pushbullet
 
 
 def getDB(sql_query):
@@ -60,51 +56,7 @@ result_det=str(getDB(sql_str_det))
 print(result_det)
 
 
-if (result_det == 'ERROR'):
-    
-    fromaddr = "notificaciones@igromi.com"
-    # instance of MIMEMultipart 
-    msg = MIMEMultipart() 
+if (1):
 
-    # storing the senders email address 
-    msg['From'] = fromaddr 
-
-    # storing the subject 
-    msg['Subject'] = "Alerta de carga de datos"
-
-    # string to store the body of the mail 
-    body = "No se han registado datos en las pasadas 3 horas."
-
-    # attach the body with the msg instance 
-    msg.attach(MIMEText(body, 'plain')) 
-
-    # creates SMTP session 
-    s = smtplib.SMTP('smtp.zoho.com', 587) 
-
-    # start TLS for security 
-    s.starttls() 
-
-    # Authentication 
-    s.login(fromaddr, "imagina12") 
-
-    # Converts the Multipart msg into a string 
-    text = msg.as_string() 
-
-    # sending the mail 
-    msg['To'] = "aguileraelectro@gmail.com",
-    s.sendmail(fromaddr,"aguileraelectro@gmail.com", text)
-
-    time.sleep(10)
-
-    msg['To'] = "victor.ruz@igromi.com"
-    s.sendmail(fromaddr, "victor.ruz@igromi.com", text) 
-
-    time.sleep(10)
-
-    msg['To'] = "cristian.ulloa@fiordoaustral.com"
-    s.sendmail(fromaddr, "cristian.ulloa@fiordoaustral.com", text) 
-
-    time.sleep(10)
-
-    # terminating the session 
-    s.quit()
+    pb = Pushbullet('o.MjdJSUG2pZPPhruHV0DfgTYRZCIldfNt')
+    push = pb.push_note('Alerta','Problema con carga de planilla Fiordo Austral')
